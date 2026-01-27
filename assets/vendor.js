869f779e34ab3789d1c980bb0b3c7f600afe552d
Jose Miguel Luna <jose2694luna@gmail.com>
2026-01-27 09:41:23 -0600

    adding categories to the stores

--- assets/vendor.js
+++ assets/vendor.js
@@ -1,9 +1,12 @@
-(()=>{function a(e){console.log("vendors:",e);const t=document.querySelector(".vendors-container ul");e.reverse().forEach(n=>{const{id:o,title:s,status:u,logo_link:f,brand_name:c,short_description:r}=n;!u||!window.SHOP_VENDORS.includes(c)||(t.classList.contains("vendors-loaded")||t.classList.add("vendors-loaded"),t.insertAdjacentHTML("afterbegin",`<li data-vendor="${o}">
-        <a data-slug="${c}" class="btn-set-vendor">
-          <img src="${f}"/>
+(()=>{function i(t){const n=document.querySelector(".vendors-container"),r=l(t);for(const e in r){console.log("Categoría:",e),n.insertAdjacentHTML("beforeend",`<div>
+          <h1>${e}</h1>
+          <ul class="stores-${e.replaceAll(" ","")}"></ul>
+        </div>`);const o=document.querySelector(`.stores-${e.replaceAll(" ","")}`);r[e].forEach(s=>{const{id:v,title:c,status:m,logo_link:p,brand_name:d,vendor_category:a}=s;(!m||!window.SHOP_VENDORS.includes(d))&&!window.SHOP_VENDORS.includes(c)||(o.classList.contains("vendors-loaded")||o.classList.add("vendors-loaded"),o.insertAdjacentHTML("beforeend",`<li data-vendor="${v}">
+        <a data-slug="${d}" class="btn-set-vendor">
+          <img src="${p}"/>
           <div class="btn-set-vendor-info">
-            <h3>${s}</h3>
-            ${r?`<p>${r}</p>`:""}
+            <h3>${c}</h3>
+            ${a?`<p>${a}</p>`:""}
           </div>
         </a>
-      </li>`))})}function i(e){document.addEventListener("click",t=>{const o=t.target.closest("a");if(o&&o.matches("a.btn-set-vendor")){const s=o.getAttribute("data-slug");l(s)}})}function d(){fetch("/a/dashboard/vendors-list?shop=txhvse-rr.myshopify.com&limit=100",{method:"GET",credentials:"include",headers:{accept:"*/*","accept-language":"es,en-US;q=0.9,en;q=0.8","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"}}).then(e=>{if(!e.ok)throw new Error("HTTP "+e.status);return e.json()}).then(e=>{a(e.data.vendors)}).catch(e=>{console.error("Error en fetch:",e)})}async function g(e){const n=await(await fetch(`/a/dashboard/vendor-products/${e}?shop=txhvse-rr.myshopify.com&sort_by=title-ascending&limit=100`,{method:"GET",credentials:"include",headers:{accept:"*/*","accept-language":"es,en-US;q=0.9,en;q=0.8","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"}})).json();if(n)return n.data}function l(e){window.location.href=`/collections/vendors?q=${e}`}function h(){i(),d()}h()})();
+      </li>`))})}}function l(t){const n=t.reduce((e,o)=>{const s=o.vendor_category||"Otros";return e[s]||(e[s]=[]),e[s].push(o),e},{});return Object.keys(n).forEach(e=>{n[e].sort((o,s)=>o.title.localeCompare(s.title))}),Object.keys(n).sort((e,o)=>e==="Otros"?1:o==="Otros"?-1:e.localeCompare(o)).reduce((e,o)=>(e[o]=n[o],e),{})}function u(t){document.addEventListener("click",n=>{const e=n.target.closest("a");if(e&&e.matches("a.btn-set-vendor")){const o=e.getAttribute("data-slug");h(o)}})}function f(){fetch("/a/dashboard/vendors-list?shop=txhvse-rr.myshopify.com&limit=100",{method:"GET",credentials:"include",headers:{accept:"*/*","accept-language":"es,en-US;q=0.9,en;q=0.8","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"}}).then(t=>{if(!t.ok)throw new Error("HTTP "+t.status);return t.json()}).then(t=>{i(t.data.vendors)}).catch(t=>{console.error("Error en fetch:",t)})}async function y(t){const r=await(await fetch(`/a/dashboard/vendor-products/${t}?shop=txhvse-rr.myshopify.com&sort_by=title-ascending&limit=100`,{method:"GET",credentials:"include",headers:{accept:"*/*","accept-language":"es,en-US;q=0.9,en;q=0.8","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"}})).json();if(r)return r.data}function h(t){window.location.href=`/collections/vendors?q=${t}`}function g(){u(),f()}g()})();
--- src/js/vendors/vendor.js
+++ src/js/vendors/vendor.js
@@ -1,25 +1,67 @@
 (() => {
   function renderVendors(vendors) {
-    console.log("vendors:", vendors);
-    const container = document.querySelector('.vendors-container ul')
+    const container = document.querySelector('.vendors-container')
+    const vendorsCategory = grouped(vendors)
 
-    vendors.reverse().forEach(vendor => {
-      const { id, title, status, logo_link, brand_name, short_description } = vendor
 
-      if (!status || !window.SHOP_VENDORS.includes(brand_name)) return
+    for (const category in vendorsCategory) {
+      console.log("Categoría:", category);
 
-      if (!container.classList.contains('vendors-loaded')) container.classList.add('vendors-loaded')
+      container.insertAdjacentHTML('beforeend', `<div>
+          <h1>${category}</h1>
+          <ul class="stores-${category.replaceAll(' ', '')}"></ul>
+        </div>`)
 
-      container.insertAdjacentHTML('afterbegin', `<li data-vendor="${id}">
+      const list = document.querySelector(`.stores-${category.replaceAll(' ', '')}`)
+
+      vendorsCategory[category].forEach(vendor => {
+        const { id, title, status, logo_link, brand_name, vendor_category } = vendor
+
+        if (!status || !window.SHOP_VENDORS.includes(brand_name)) {
+          if (!window.SHOP_VENDORS.includes(title)) return
+        }
+
+        if (!list.classList.contains('vendors-loaded')) list.classList.add('vendors-loaded')
+
+        list.insertAdjacentHTML('beforeend', `<li data-vendor="${id}">
         <a data-slug="${brand_name}" class="btn-set-vendor">
           <img src="${logo_link}"/>
           <div class="btn-set-vendor-info">
             <h3>${title}</h3>
-            ${short_description ? `<p>${short_description}</p>` : ''}
+            ${vendor_category ? `<p>${vendor_category}</p>` : ''}
           </div>
         </a>
       </li>`)
+      });
+    }
+  }
+
+  function grouped(vendors) {
+    const grouped = vendors.reduce((acc, vendor) => {
+      const category = vendor.vendor_category || "Otros"; // usar Otros
+      if (!acc[category]) acc[category] = [];
+      acc[category].push(vendor);
+      return acc;
+    }, {});
+
+    // 2. Ordenar vendors dentro de cada categoría
+    Object.keys(grouped).forEach(category => {
+      grouped[category].sort((a, b) => a.title.localeCompare(b.title));
     });
+
+    // 3. Ordenar categorías alfabéticamente excepto "Otros" que va al final
+    const orderedGrouped = Object.keys(grouped)
+      .sort((a, b) => {
+        if (a === "Otros") return 1;   // mover "Otros" hacia abajo
+        if (b === "Otros") return -1;
+        return a.localeCompare(b);     // resto alfabético
+      })
+      .reduce((acc, category) => {
+        acc[category] = grouped[category];
+        return acc;
+      }, {});
+
+    return orderedGrouped
   }
 
   function addEvents(e) {
